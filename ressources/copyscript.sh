bd6e0cd0bb02e07e24a83416cc4001a02d876a45
Lenard Frommelt <lenny09918050@gmail.com>
2023-03-22 16:38:57 +0300

    1

--- ressources/copyscript.sh
+++ ressources/copyscript.sh
@@ -29,7 +29,7 @@ cp sources/ressources/iua_course_2022.pdf output/
 cp sources/ressources/euro_thrombosis_2022.png output/
 cp sources/ressources/dga_summer_school_2022.pdf output/
 cp sources/ressources/ddg_herbsttagung.png output/
-cp sources/ressources/oegim_jahrestagung_2022.pdf output/
+cp sources/ressources/oegim_jahrestagung_2023.pdf output/
 cp sources/ressources/gerinnungstage_2023.pdf output/
 
 echo 0
